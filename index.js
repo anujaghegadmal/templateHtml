8142475dfdd2038ae6fe28d1dd123368a26f0461
Anuja Ghegadmal <anujag2411@gmail.com>
2021-12-22 15:15:04 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+playBtn.onclick = function clicked(){
+    console.log("\"Play button\" clicked!");
+    location.href = "https://www.youtube.com/watch?v=k5E2AVpwsko";
+}
+
+function btnClicked(){
+    console.log("\"Download Dashlane\" button clicked!");
+}
